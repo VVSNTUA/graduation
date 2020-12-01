b9999e27bdbb850442714b4a37ac61b108573c8e
Vivi Sun <sun20317@gmail.com>
2020-12-01 18:09:58 +0800

    added cubeSprite that is draggable

--- app.js
+++ app.js
@@ -1,46 +1,135 @@
+
+// canvas information
+let canvasWidth = 1000;
+let canvasHeight = 600;
+// test
 let img;
-let cnv;
+// mouse interaction
+let draggedSprite;
+// modify into json in future
+let cubeSprite;
+let cubeSprite_2;
+const cubeSpriteData = [
+  {
+    layer:1,
+    name:"back",
+    animation:[
+      {
+        animationLabel: "spin",
+        filePath:["assets/cube_spin_back_00001.png","assets/cube_spin_back_00012.png"]
+      },{
+        animationLabel: "transform",
+        filePath:["assets/cube_transform_front_00001.png","assets/cube_transform_front_00010.png"]
+      }
+    ],
+    
+  },
+  {
+    layer:2,
+    name:"heart",
+    animation:[
+      {
+        animationLabel: "nutral",
+        filePath:["assets/cube_heart_nutral_00001.png","assets/cube_heart_nutral_00004.png"]
+      }
+    ]
+    
+  },
+  {
+    layer:3,
+    name:"front",
+    animation:[
+      {
+        animationLabel: "spin",
+        filePath:["assets/cube_spin_front_00001.png","assets/cube_spin_front_00012.png"]
+      }
+    ]
+    
+  },
+  
+]
 
-let c1_idle;
-let c1_click;
-let c1_transition;
 
-let c1 = {
-    idle: 0,
-    click: 0,
-    transition: 0
-}
-
-let current_gif;
 
 function preload(){
-    c1.idle = loadImage("img/c1-idle-placeholder.gif");
-    c1.click = loadImage("img/c1-click-placeholder.gif");
-    c1.transition = loadImage("img/c1-transition-placeholder.gif");
-    console.log(c1);
 
-    current_gif = c1.idle;
-}
-function setup(){
-    cnv = createCanvas(windowWidth,windowHeight);
-    cnv.parent('app-container');
-    cnv.mousePressed(next);
-    background(200,200,200);
-    
-}
-function draw(){
-    image(current_gif,
-        (cnv.width-current_gif.width)/2,
-        (cnv.height-current_gif.height)/2,
-        current_gif.width,
-        current_gif.height);
+    img = loadImage("assets/cube_transform_front_00001.png");
+    cubeSprite_2 = createSprite(canvasWidth/2 ,canvasHeight/2,300,300);
+    cubeSprite_2.addAnimation("transform","assets/cube_transform_front_00001.png","assets/cube_transform_front_00010.png");
+    // cubeSprite_2.addImage("bruh",img);
+    // cubeSprite_2.changeImage("bruh");
+    cubeSprite_2.animation.frameDelay = 60;
+
+
+
+
+
+
+    // Create cubeSprite
+    cubeSprite = new Group();
+    // create sub sprites
+    for( let i = 1; i <= 3; i++){
+      let c = cubeSpriteData[i-1];
+      
+      let s = createSprite(canvasWidth/2 ,canvasHeight/2,300,300);  // sub sprite
+
+      // add animations
+      for (let j = 0; j < c.animation.length; j++){
+        let anim = c.animation[j];
+        let label = anim.animationLabel;
+        let files = anim.filePath;
+        s.addAnimation( label, files[0], files[files.length - 1]);
+      }
+      // rescale
+      s.scale = 1;
+      s.addToGroup(cubeSprite);
+    }
+
+
+
+    // add collider to front layer
+    // should detect pixel instead
+    cubeSprite[0].setDefaultCollider();
+    // assign functions responding to mouse events
+    // when mouse is clicked, front and back sprite (cubeSprite[0], cubeSprite[2])
+    // will stop playing
+
+    cubeSprite[0].onMousePressed = function(){
+        if(draggedSprite == null){
+          draggedSprite = cubeSprite;
+        }
+    }
+
+    cubeSprite[0].onMouseReleased = function(){
+
+        if(draggedSprite == cubeSprite){
+          draggedSprite = null;
+        }
+    }
+
 }
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-  }
+function setup() {
+    createCanvas(canvasWidth,canvasHeight);
 
-function next(){
-    current_gif = c1.click;
+}
+  
+function draw() {
+    background(245,245,245); 
+    // image(img,530,530);
+
+
+    if(draggedSprite != null){
+      draggedSprite.forEach((sprite) => {
+        sprite.position.x = mouseX;
+        sprite.position.y = mouseY;
+      }
+      )
+    }
+    cubeSprite[0].debug = mouseIsPressed;
+    // cubeSprite_2.degug = mouseIsPressed;
+    drawSprites(cubeSprite);
+    // drawSprite(cubeSprite_2);
 }
 
+ 
