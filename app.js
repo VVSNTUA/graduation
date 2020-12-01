ab621d3e2b56b4bfbab8a165c953ae2d4886eaec
VVSNTUA <33819714+VVSNTUA@users.noreply.github.com>
2020-12-02 04:40:33 +0800

    press any button to play cubeSprite animations

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ let canvasHeight = 600;
 // test
 let img;
 let testSprite;
+let currentState = 1;
 // mouse interaction
 let draggedSprite;
-// modify into json in future
+// modify into json in the future
 let cubeSprite;
 const cubeSpriteData = [
   {
@@ -50,6 +51,9 @@ const cubeSpriteData = [
       },{
         animationLabel: "transform",
         filePath:["assets/cube_front_transform_00001.png","assets/cube_front_transform_00010.png"]
+      },{
+        animationLabel: "crack",
+        filePath:["assets/cube_front_crack_00001.png","assets/cube_front_crack_00004.png"]
       }
     ]
   },
@@ -116,7 +120,7 @@ function setup() {
 }
   
 function draw() {
-    background(245,245,245); 
+    background(220,220,220); 
     // image(img,530,530);
 
 
@@ -150,7 +154,7 @@ function draw() {
 function createTestSprite(){
       // img = loadImage("assets/cube_crack_front_00004.png");
       testSprite = createSprite(canvasWidth/2 ,canvasHeight/2,300,300);
-      testSprite.addAnimation("transform","assets/cube_back_transform_00001.png","assets/cube_back_transform_00010.png");
+      testSprite.addAnimation("crack","assets/cube_back_transform_00001.png","assets/cube_back_transform_00010.png");
       // testSprite.addImage("bruh",img);
       // testSprite.changeImage("bruh");
       testSprite.animation.frameDelay = 10;
@@ -160,12 +164,73 @@ function createTestSprite(){
 
 function keyPressed() {
 
-  // console.log(cubeSprite);
 
+
+  switch (currentState) {
+    case 1:
+      playCrack();
+      currentState = 2;
+      break;
+    case 2:
+      playTransform();
+      currentState = 3;
+      break;
+    case 3:
+      playSpin();
+      currentState = 1;
+      break;
+    default:
+      console.log(`currentState is ${currentState} and it is not handled`);
+      break;
+  }
+ 
+
+}
+
+function playSpin() {
+  // * Set cubeSprite to spin
+// * cubeSprite[0] back  => animation "spin"
+// * cubeSprite[1] heart => animation "nutral"
+// * cubeSprite[2] front => animation "spin"
+
+cubeSprite[0].changeAnimation("spin");
+cubeSprite[0].animation.play();
+cubeSprite[1].changeAnimation("nutral");
+cubeSprite[2].changeAnimation ("spin");
+}
+
+
+
+function playCrack() {
+  // * Set cubeSprite to crack
+// * cubeSprite[0] back  => image "cube_back_spin_000001.png"
+// * cubeSprite[1] heart => animation "nutral"
+// * cubeSprite[2] front => animation "crack" frame delay 10
+
+cubeSprite[0].animation.stop();
+cubeSprite[0].animation.changeFrame(0);
+cubeSprite[2].changeAnimation("crack");
+cubeSprite[2].animation.frameDelay = 10
+}
+
+function playTransform() {
+
+    // * Set cubeSprite to transform
+  // * 
+  // * cubeSprite[0] back  => animation "transform" noLoop
+  // * cubeSprite[1] heart => animation "transform" noLoop
+  // * cubeSprite[2] front => animation "transform" noLoop
+  // * 
   cubeSprite.forEach((sprite)=>{
-    console.log(sprite);
     sprite.changeAnimation("transform");
     sprite.animation.frameDelay = 10;
   });
+}
 
+function playNutral(){
+  // TODO
+}
+
+function playSqueeze(){
+  // TODO
 }
